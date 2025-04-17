ad257ca36ca1a840f4c58f754869d512b811d5eb
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2025-04-17 12:48:39 +0530

    Update [proxy].edge.js

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,22 +1,7 @@
-const RSC_REDIRECT_PATHS = [
-    '/about','/projects','/skills'
-  ];
-  const RSC_HEADER = 'rsc';
-  const RSC_HEADER_VALUE = '1';
-  const RSC_QUERY_PARAM = 'rsc';
+
   
   export default function handler(request) {
-    const parsedUrl = new URL(request.url);
-    const route = parsedUrl.pathname;
-    const rscQueryParamExists = !!parsedUrl.searchParams.get(RSC_QUERY_PARAM);
-    const rscHeaderExists = request.headers.get(RSC_HEADER) === RSC_HEADER_VALUE;
-    const isRscRedirectPath = RSC_REDIRECT_PATHS.indexOf(route) > -1;
-  
-    if (isRscRedirectPath && !rscQueryParamExists && rscHeaderExists) {
-        const modifiedRequest = new Request(request);
-        modifiedRequest.headers.delete(RSC_HEADER);
-        return fetch(modifiedRequest);
-    }
+    
     return fetch(request);
   }
 
