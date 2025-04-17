d42f98f3951e62cd8d90f7f960253e174a50104a
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2025-04-17 13:05:15 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'no-store', 
+            value: 's-maxage=300', 
           },
         ],
       },
